2d51f4325dba4a91212b597bde81599fe90c6995
Ethan Martin <60288171+yuckdevchan@users.noreply.github.com>
2024-05-27 14:30:36 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,143 @@
+from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QStyleFactory, QTextEdit, QToolBar, QFileDialog, QLineEdit, QMessageBox
+import sys, os, argparse
+
+from PySide6.QtGui import QColor, QTextCharFormat, QFont, QSyntaxHighlighter
+from PySide6.QtCore import Qt, QRegularExpression, QTimer
+
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import HtmlFormatter
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        parser = argparse.ArgumentParser()
+        parser.add_argument("path", nargs="?", default=None)
+        args = parser.parse_args()
+        self.path = args.path
+
+        self.initUI()
+        self.update_path_text()
+
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.update_highlight)
+        self.timer.start(100)
+
+    def initUI(self):
+        self.setWindowTitle("splatpad")
+        self.setGeometry(0, 0, 500, 600)
+
+        self.layout = QVBoxLayout()
+
+        self.toolbar = QToolBar()
+        self.toolbar.addAction("save")
+        self.toolbar.actions()[0].setShortcut("Ctrl+S")
+        self.toolbar.actions()[0].triggered.connect(self.save)
+        self.toolbar.addAction("save as")
+        self.toolbar.actions()[1].setShortcut("Ctrl+Shift+S")
+        self.toolbar.actions()[1].triggered.connect(self.save_as)
+        self.toolbar.addAction("open")
+        self.toolbar.actions()[2].setShortcut("Ctrl+O")
+        self.toolbar.actions()[2].triggered.connect(self._open)
+        self.toolbar.addAction("copy all")
+        self.toolbar.actions()[3].triggered.connect(self.copy_all)
+        self.toolbar.addAction("paste all")
+        self.toolbar.actions()[4].triggered.connect(self.paste_all)
+        self.toolbar.addAction("clear")
+        self.toolbar.actions()[5].triggered.connect(lambda: self.text_box.clear())
+        self.toolbar.addAction("delete")
+        self.toolbar.actions()[6].triggered.connect(self.delete_file)
+        self.toolbar.addAction("about")
+        self.toolbar.actions()[7].triggered.connect(lambda: QMessageBox.information(self, "About", "splatpad\n\nA simple Qt text editor."))
+        self.layout.addWidget(self.toolbar)
+
+        self.text_box = QTextEdit()
+        self.text_box.setAcceptRichText(False)
+        self.text_box.setPlaceholderText("Start typing here...")
+        self.text_box.setHtml(highlight(self.text_box.toPlainText(), PythonLexer(), HtmlFormatter()))
+        self.layout.addWidget(self.text_box)
+
+        self.path_text = QLineEdit()
+        self.path_text.returnPressed.connect(self.open_path)
+        self.layout.addWidget(self.path_text)
+
+        self.setLayout(self.layout)
+        
+        if self.path != None:
+            with open(self.path, "r") as f:
+                self.text_box.setText(f.read())
+
+        self.show()
+
+    def update_highlight(self):
+        pass
+
+    def update_path_text(self):
+        if self.path == None:
+            self.path_text.setPlaceholderText("No file path")
+            font = self.path_text.font()
+            font.setItalic(True)
+            self.path_text.setFont(font)
+        else:
+            self.path_text.setText(self.path)
+
+    def save(self):
+        if self.path == None:
+            chosen_path = QFileDialog.getSaveFileName(self, "Save file", "", "Text files (*)")[0]
+            if chosen_path != "":
+                self.path = chosen_path
+            self.update_path_text()
+            with open(self.path, "w") as f:
+                f.write(self.text_box.toPlainText())
+        else:
+            with open(self.path, "w") as f:
+                f.write(self.text_box.toPlainText())
+
+    def save_as(self):
+        chosen_path = QFileDialog.getSaveFileName(self, "Save file", "", "Text files (*)")[0]
+        if chosen_path != "":
+            self.path = chosen_path
+        self.update_path_text()
+        with open(self.path, "w") as f:
+            f.write(self.text_box.toPlainText())
+
+    def _open(self):
+        chosen_path = QFileDialog.getOpenFileName(self, "Open file", "", "Text files (*)")[0]
+        if chosen_path != "" and chosen_path != None:
+            self.path = chosen_path
+            self.update_path_text()
+        
+            with open(self.path, "r") as f:
+                self.text_box.setText(f.read())
+
+    def open_path(self):
+        self.path = self.path_text.text()
+        self.update_path_text()
+        with open(self.path, "r") as f:
+            self.text_box.setText(f.read())
+
+    def copy_all(self):
+        QApplication.clipboard().setText(self.text_box.toPlainText())
+
+    def paste_all(self):
+        self.text_box.setText(QApplication.clipboard().text())
+
+    def delete_file(self):
+        if self.path != None and os.path.exists(self.path):
+            warning = QMessageBox.warning(self, "Delete file", "Are you sure you want to delete the file?", QMessageBox.Yes | QMessageBox.No)
+            if warning == QMessageBox.Yes:
+                if self.path != None:
+                    os.remove(self.path)
+                    self.path = None
+                    self.update_path_text()
+                    self.text_box.clear()
+        else:
+            QMessageBox.warning(self, "Delete file", "No file to delete", QMessageBox.Ok)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    app.setStyle(QStyleFactory.create("Windows"))
+    main_window = MainWindow()
+    app.processEvents()
+    sys.exit(app.exec())
